15edd90a628c43cddae3752196b87ffe3313a862
godu <datogoduadze704@gmail.com>
2026-03-27 22:39:29 +0400

    ..

--- KitchenTabletApp/src/app/core/app-config.ts
+++ KitchenTabletApp/src/app/core/app-config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
-  apiBaseUrl: 'http://192.168.0.103:5223/api',
-  hubUrl: 'http://192.168.0.103:5223/hubs/kitchen',
+  apiBaseUrl: 'https://qrmenu-backend-api.onrender.com/api',
+  hubUrl: 'https://qrmenu-backend-api.onrender.com/hubs/kitchen',
   defaultRestaurantId: 'a2759f83-f410-423d-82de-f4d7538325a3'
 };
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://qrmenu-api-l29d.onrender.com/api',
-  baseUrl: 'https://qrmenu-api-l29d.onrender.com'
+  apiUrl: 'https://qrmenu-backend-api.onrender.com/api',
+  baseUrl: 'https://qrmenu-backend-api.onrender.com'
 };
