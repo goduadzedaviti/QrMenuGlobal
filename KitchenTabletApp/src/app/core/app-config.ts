fa254907e091add06e38974031297f7f0c3e0ca7
godu <datogoduadze704@gmail.com>
2026-03-27 22:35:06 +0400

    code init

---
+++ KitchenTabletApp/src/app/core/app-config.ts
@@ -0,0 +1,5 @@
+export const appConfig = {
+  apiBaseUrl: 'http://192.168.0.103:5223/api',
+  hubUrl: 'http://192.168.0.103:5223/hubs/kitchen',
+  defaultRestaurantId: 'a2759f83-f410-423d-82de-f4d7538325a3'
+};
